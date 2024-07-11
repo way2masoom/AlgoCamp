7d80214a2a36f1f6074ae93eb3b857920376f89f
way2masoom <way2masoom@gmail.com>
2024-07-11 09:09:34 +0545

    simple Button click by Js, and variable in js is started

---
+++ JavaScript/first.js
@@ -0,0 +1,5 @@
+// js code
+
+document.getElementById("myButton").addEventListener("click", function() {
+    document.getElementById("demo").style.fontSize = "24px"; // Set the font size to 24px
+});
