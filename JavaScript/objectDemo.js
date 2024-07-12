96a88a14a5e105e5b99391e8a3cc26f3c60e9b32
way2masoom <way2masoom@gmail.com>
2024-07-12 08:26:04 +0545

    add demo of object

---
+++ JavaScript/objectDemo.js
@@ -0,0 +1,19 @@
+// Object data Types in JS
+
+let product={
+    productName:"Iphone 13 pro Max",
+    productPrice:155000,
+    productDiscount:15,
+    isAvailable:true
+};
+
+let Social={
+    facebook:1000,
+    inst:1059,
+    x:1366,
+    linkedin:35883
+
+};
+
+console.log(product);
+console.log(Social);
