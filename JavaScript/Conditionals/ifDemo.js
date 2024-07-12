99e4c236a5a78fdfd9a729af656c13b11793d676
way2masoom <way2masoom@gmail.com>
2024-07-13 00:31:52 +0545

    if demo is added

---
+++ JavaScript/Conditionals/ifDemo.js
@@ -0,0 +1,16 @@
+// If else in JS
+
+// simple example of User login Website
+console.log("Welcome... to Amazon Prime");
+
+let isUserSignedIn=true;    // This denote the user is SignedIn to the site
+let isPaidUser=true;
+
+if (isUserSignedIn==true && isPaidUser==true) {
+    //This part of code only executed if the user is signedIN
+    console.log("Showing the Dashboard ! 🎬");
+} else {
+    console.log("Show the Home page 🏠");
+}
+
+console.log("End..")
