3cece0cb0590190904f5e4375a9bf87407813306
way2masoom <way2masoom@gmail.com>
2024-07-12 08:50:50 +0545

    assignment operators added

---
+++ JavaScript/operators/AssignmentOperators.js
@@ -0,0 +1,18 @@
+// Assignment Operators in JS
+
+let x=10;
+
+x+=5;
+console.log("Result is : ",x);
+
+x-=5;
+console.log("Result is : ",x);
+
+x*=5;
+console.log("Result is : ",x);
+
+x/=5;
+console.log("Result is : ",x);
+
+x%=5;
+console.log("Result is : ",x);
