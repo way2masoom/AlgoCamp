c5b1c971d988f7d64bfcdd301f1c248a588d2dd2
way2masoom <way2masoom@gmail.com>
2024-07-12 23:55:21 +0545

    comparison operator

---
+++ JavaScript/operators/comparisonOperators.js
@@ -0,0 +1,16 @@
+//Comparison JS
+console.log(10<21);
+console.log(10>20);
+
+console.log(8<=11);
+console.log(9>=23);
+
+// Equal to Operators
+console.log(10==10);
+console.log("10" ==10);
+
+console.log(10===10);
+console.log("10" ===10);
+
+console.log(10!==10);
+console.log("10" !==10);
