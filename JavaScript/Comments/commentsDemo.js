a0fb7e7cabb141cde8132cb5575344646fbae6ca
way2masoom <way2masoom@gmail.com>
2024-07-13 00:00:21 +0545

    demo of comments is added

---
+++ JavaScript/Comments/commentsDemo.js
@@ -0,0 +1,12 @@
+// comments in JS 
+
+let a=4,b=6;
+console.log("Sum + operator",a+b); // addition of two number
+console.log("Subtract - operator",a-b); // subtraction of two number 
+
+/* Arithmetic Operators 
+    example used in the bellow 
+    operation */
+console.log("Multiply * operator",a*b);
+console.log("Modulo % operator",a%b);
+console.log("Divide / operator",a/b);
