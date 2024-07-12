a6985e741b1e0bf0ac83509e96a631e57d11ae97
way2masoom <way2masoom@gmail.com>
2024-07-13 01:08:59 +0545

    if else condition exam is added

---
+++ JavaScript/03_Conditionals/ifelseDemo.js
@@ -0,0 +1,15 @@
+// if else example and learning in js 
+
+// let UserSubscription = "UnSubscribe";
+// let UserSubscription = "super";
+let UserSubscription = "premium";
+
+if (UserSubscription == "Unsubscribe") {
+    console.log("SHow price Page")
+} else if (UserSubscription == "super") {
+    console.log("Only hindi Content")
+} else {
+    console.log("Show all Content");
+}
+
+console.log("End...");
