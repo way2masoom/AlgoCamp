32e1d367366e45c05f099d425b04311281f8dc80
way2masoom <way2masoom@gmail.com>
2024-07-13 11:41:30 +0545

    bitwise operators added

---
+++ JavaScript/01_operators/BitwiseOperators.js
@@ -0,0 +1,16 @@
+// Bitwise Operators in JS
+
+const a=5;
+const b=6;
+
+console.log("Bitwise AND",a&b);
+console.log("Bitwise OR",a|b);
+console.log("Bitwise XOR",a^b);
+console.log("Bitwise NOT",~b);
+console.log("Bitwise NOT",~a);
+
+console.log("1 left shift 4 =",1<<4);
+console.log("4 left shift 3 =",4<<3);
+
+console.log("27 right shift 2 =",27>>2);
+console.log(12>>1);
