ffc56ee094b00da38d27c6146e793f74fbc07c4b
way2masoom <way2masoom@gmail.com>
2024-07-27 23:25:38 +0530

    simple string concept

---
+++ JavaScript/05_FunctionBasics/Functions11.js
@@ -0,0 +1,16 @@
+// Ways to Define Strings
+
+let s1="Hello My name is MD";
+
+let s2="What is your name?";
+
+let s3="My name is rohit";
+
+let s4='Md Asked my name "So i answer my name is Rohit"';
+
+console.log(s1,s2,s3);
+console.log(s4);
+
+let x=10;
+let s5=`Value of x is = ${x}`; // templated String
+console.log(s5);
