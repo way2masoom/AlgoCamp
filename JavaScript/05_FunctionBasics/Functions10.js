67d146dfe2294d257c94fcfb23d16bbb7503673d
way2masoom <way2masoom@gmail.com>
2024-07-27 23:03:52 +0530

    function Date features

---
+++ JavaScript/05_FunctionBasics/Functions10.js
@@ -0,0 +1,13 @@
+// creating a date in JS 
+
+const d=new Date();
+
+console.log(d); 
+
+console.log("Date is ",d.getDate());
+console.log("Day is ",d.getDay());
+console.log("Months is ",d.getMonth());
+console.log("Year is ",d.getFullYear());
+console.log("Second is",d.getSeconds());
+console.log("Minute is ",d.getMinutes());
+console.log("Hours is",d.getHours());
