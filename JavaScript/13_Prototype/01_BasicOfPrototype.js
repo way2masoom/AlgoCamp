359ccc5ae6afa32076271b9b9e5b3d73add9d185
way2masoom <way2masoom@gmail.com>
2024-08-28 23:39:48 +0530

    Update example

--- JavaScript/13_Prototype/01_BasicOfPrototype.js
+++ JavaScript/13_Prototype/01_BasicOfPrototype.js
@@ -18,3 +18,14 @@ let obj = new Product("Iphone",1299);
 console.log(obj);
 
 console.log(Product.prototype);
+
+// use of dunder proto
+// Creating a new object that will inherit from Product
+let electronicProduct = {
+    category: 'Electronics'
+};
+
+// Linking electronicProduct's prototype to Product's prototype using __proto__
+electronicProduct.__proto__ = new Product("Laptop", 1500);
+
+console.log(electronicProduct.__proto__); // Output: Product { name: 'Laptop', price: 1500 }
