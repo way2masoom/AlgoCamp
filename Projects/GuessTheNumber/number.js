fc9f61896d2efed69f835a1089129d90ecc01d32
way2masoom <way2masoom@gmail.com>
2024-12-08 20:26:53 +0530

    Collected the data and made a over view for the games

--- Projects/GuessTheNumber/number.js
+++ Projects/GuessTheNumber/number.js
@@ -1,1 +1,51 @@
 console.log("hello");
+
+// Generating a random value 
+const randomNumber=parseInt(Math.random()*100+1);
+
+const submit=document.querySelector('#subt');
+const UserInput=document.querySelector('#guessField');
+
+const guessSlot=document.querySelector('.guesses');
+const remaining=document.querySelector('.lastResult');
+
+const lowOrhi=document.querySelector('.lowOrHi');
+const startOver=document.querySelector('.resultParas');
+
+const p=document.createElement('p');
+
+let prevGuess=[];
+let numGuess=1;
+let playGame=true;
+
+// Function for validation  the number is between 1 to 100
+function validateGuess(guess){
+    //
+
+}
+
+// Checking the guess (To print the right value)
+function checkGuess(guess){
+    //
+
+}
+
+// I will clean the value and display, Update the Guess number
+function displayGuess(guess){
+    //
+}
+
+function displayMessage(messages){
+    // 
+}
+
+// End and newGame 
+function endGame(){
+    //
+}
+
+function newGame(){
+    //
+}
+
+
