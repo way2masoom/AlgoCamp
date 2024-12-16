7b423d58050008f7f5241c1d40a5284e3e46cb73
way2masoom <way2masoom@gmail.com>
2024-12-16 21:59:02 +0530

    closure definition

--- JavaScript/15_asyncJS/05_ClosureInJS.js
+++ JavaScript/15_asyncJS/05_ClosureInJS.js
@@ -1,4 +1,10 @@
 // Closure in Js
+// A closure is the combination of a function bundled together (enclosed) 
+// with references to its surrounding state (the lexical environment). 
+// In other words, a closure gives a function access to its outer scope. 
+// In JavaScript, closures are created every time a function is created, at function creation time.
+
+
 function fun(c,d){
     let m=10;
     // another function 
