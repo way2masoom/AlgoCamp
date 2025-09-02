2dfdf3ec8bbc08d284bbd76c68846c6f0ec228e2
MD Masoom Alam <101356833+way2masoom@users.noreply.github.com>
2025-09-02 15:44:54 +0545

    fixed the code

--- JavaScript/09_FunctionEpressions/03_Name&AnonymousFn.js
+++ JavaScript/09_FunctionEpressions/03_Name&AnonymousFn.js
@@ -2,16 +2,18 @@
 
 // Program to find the factorial of the Array element
 
-const arr=[1,2,3,4,5];
-arr.map(function factorial(n){
-    if(n==1) return 1;
-    return n*factorial(n-1);
-});
+const arr = [1, 2, 3, 4, 5];
+console.log(
+    arr.map(function factorial(n) {
+        if (n == 1) return 1;
+        return n * factorial(n - 1);
+    }));
 
 
-// Recursion function 
-function fact(n){
-    if(n=1) return 1;
-return n*fact(n-1);
+const arr2 = [2, 4, 6, 8];
+// Recursion function  || Anonymous function
+function fact(n) {
+    if (n == 1) return 1;
+    return n * arguments.callee(n - 1);
 }
-console.log(arr.map(fact));
+console.log(arr2.map(fact));
